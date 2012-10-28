74d2e8254676f9f598d86a50e6ce35f3a132d17c
Jon Knapp <thann@linux.com>
2012-10-27 22:20:15 -0700

    added 'about' dialog

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -1,2 +1,5 @@
-Eye icon by <a href='http://thenounproject.com/'>the noun project</a><br>
-Color select icon by <a href='http://p.yusukekamiyamane.com/'>Yusuke Kamiyamane</a>. All rights reserved.
+<div id='about-view'>
+<h3>Attributation</h3>
+ - Eye icon by <a style='outline:none;' href='http://thenounproject.com/'>the noun project</a><br>
+ - Color select icon by <a href='http://p.yusukekamiyamane.com/'>Yusuke Kamiyamane</a>. All rights reserved.
+</div>
--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -1,6 +1,17 @@
 <link rel="stylesheet" type="text/css" href='<?php echo css_url()."footer.css"; ?>'/>
 <div class="footer">
 	<span id='copyright'> &copy; 2012 Jonathan Knapp</span>
-	<span id='about'>about</span>
+	<span id='about-link'>about</span>
 	<span id='render-time'>Page rendered in <strong>{elapsed_time}</strong> seconds.</span>
 </div>
+<?php $this->load->view('about_view.php'); ?>
+<script type='text/javascript'>
+	$("#about-view").dialog({
+		autoOpen: false,
+		title: "About Tascal",
+		width: 470,
+	});
+	$("#about-link").click(function() {
+		$("#about-view").dialog("open")
+	});
+</script>
