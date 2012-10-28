f35859765c40a24007e812a549cd1c9d74848e35
Jon Knapp <thann@linux.com>
2012-10-27 22:43:51 -0700

    about page links open in new tab

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -1,6 +1,6 @@
 <div id='about-view'>
 <h3>Attributation</h3>
 <!-- - Eye icon by <a href='http://thenounproject.com/'>the noun project</a><br>-->
- - Eye icon shamlessly stolen from jqueryui.com<br>
- - Color select icon by <a href='http://p.yusukekamiyamane.com/'>Yusuke Kamiyamane</a>. All rights reserved.
+ - Eye icon shamlessly stolen from <a href='http://jqueryui.com/' target="_blank">jqueryui.com</a><br>
+ - Color select icon by <a href='http://p.yusukekamiyamane.com/' target="_blank">Yusuke Kamiyamane</a>. All rights reserved.
 </div>
