3df21abdcbb8d90492f6609c00872039f1a075ce
Jon Knapp <thann@linux.com>
2012-11-07 23:54:15 -0800

    added temp tzdata to xapi::ical

--- application/views/ical_view.php
+++ application/views/ical_view.php
@@ -10,7 +10,8 @@ $output = "BEGIN:VCALENDAR\r\n".
 	"METHOD:PUBLISH\r\n".
 	"PRODID:-//Jonathan Knapp//Tascal//EN\r\n".
 	"X-WR-CALNAME:".$group->title."\r\n".
-	"CALSCALE:GREGORIAN\r\n";
+	"CALSCALE:GREGORIAN\r\n".
+	"X-WR-TIMEZONE:America/Los_Angeles\r\n";
 
 //loop over tasks
 foreach ($group->tasks as $t) {
@@ -19,10 +20,10 @@ foreach ($group->tasks as $t) {
 		$output .=
 			"BEGIN:VEVENT\r\n" .
 			"SUMMARY:" . $t->title . "\r\n".
-			"UID:" . $event->eid ."\r\n".
+			"UID:" . $group->gid."-".$t->tid."-".$event->eid ."\r\n".
 			//"STATUS:" . "Confirmed" . "\r\n".
-			"DTSTART:" . date(DATE_ICAL, strtotime($event->start)) . "\r\n".
-			"DTEND:" . date(DATE_ICAL, strtotime($event->end)) . "\r\n".
+			"DTSTART;TZID=America/Los_Angeles:" . date(DATE_ICAL, strtotime($event->start)) . "\r\n".
+			"DTEND;TZID=America/Los_Angeles:" . date(DATE_ICAL, strtotime($event->end)) . "\r\n".
 			//"LAST-MODIFIED:" . date(DATE_ICAL) . "\r\n".
 			//"LOCATION:" . "locationString" ."\r\n".
 			"END:VEVENT\r\n";
