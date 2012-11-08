6852ff0a5b059bbb9dfd0174ab8a16fc501a2826
Jon Knapp <thann@linux.com>
2012-11-07 17:29:43 -0800

    initialized controller external_api

---
+++ application/controllers/external_api.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class External_Api extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('user');
+	}
+
+	function index() {
+		$ret = $this->input->post();
+		echo json_encode(array('msg'=>"LAME"));
+	}
+}
