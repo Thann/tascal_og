800d4cf36db7a6404b6c80d86c162c231b690d71
Jon Knapp <thann@linux.com>
2012-11-15 01:27:23 -0800

    changed jquery-ui theme

--- application/config/branding.php
+++ application/config/branding.php
@@ -4,7 +4,7 @@ $config['site_name'] = "tascal";
 
 $config['js_jquery'] = "libs/jquery-1.8.1.min.js"; //1.8.2 causes problems with fullcal
 $config['js_jquery-ui'] = "libs/jquery-ui-1.9.1.custom.min.js";
-$config['css_jquery-ui'] = "libs/ui-greenbear/jquery-ui-1.9.1.custom.min.css";
+$config['css_jquery-ui'] = "libs/ui-rene/jquery-ui-1.9.1.custom.min.css";
 
 /* End of file branding.php */
 /* Location: ./application/config/branding.php */
